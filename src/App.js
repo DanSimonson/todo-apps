ef23c3d31cbb510c526c939ea75c78ad9cca9148
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2019-01-31 06:37:49 +0800

    added files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/header'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>app</h1>
+        <Header />
       </div>
     );
   }
